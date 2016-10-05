23a9ad4eac4712641b1f5ad3598e5a0d23e98d96
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-10-05 11:31:11 -0700

    need to make right side symetrical

--- drawingproject.js
+++ drawingproject.js
@@ -12,6 +12,48 @@ function drawRectangle(color, xCoord, yCoord) {
 }
 drawRectangle("", 10, 10);
 
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.moveTo(100,300);
+canvas.lineTo(260,290);
+canvas.lineTo(240,350);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.moveTo(400,700);
+canvas.lineTo(260,290);
+canvas.lineTo(240,350);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.moveTo(700,300);
+canvas.lineTo(260,290);
+canvas.lineTo(240,350);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.moveTo(100,300);
+canvas.lineTo(660,290);
+canvas.lineTo(640,350);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.moveTo(700,300);
+canvas.lineTo(560,290);
+canvas.lineTo(540,350);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.moveTo(400,700);
+canvas.lineTo(620,290);
+canvas.lineTo(640,350);
+canvas.fill();
+
 
 
 canvas.beginPath();
@@ -28,6 +70,14 @@ canvas.beginPath();
 canvas.fillStyle = "orangered";
 canvas.arc(400, 400, 40, 0, 2*Math.PI);
 canvas.fill();
+
+
+
+
+
+
+
+
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
@@ -49,6 +99,11 @@ canvas.moveTo(520,270);
 canvas.lineTo(450,300);
 canvas.lineTo(500,345);
 canvas.fill();
+
+
+
+
+
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
