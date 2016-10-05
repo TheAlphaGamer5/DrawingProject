0db4d03505457c638f6b349d232e0b6a71167184
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-10-04 19:36:02 -0700

    Crest of Courage
    slowly coming together

--- drawingproject.js
+++ drawingproject.js
@@ -10,21 +10,27 @@ function drawRectangle(color, xCoord, yCoord) {
   canvas.fillRect(xCoord, yCoord, 800, 800);
 
 }
-drawRectangle("green", 10, 10);
+drawRectangle("", 10, 10);
 
 
 
 canvas.beginPath();
-canvas.fillStyle = "black";
+canvas.fillStyle = "orange";
 canvas.arc(400, 400, 100, 0, 2*Math.PI);
 canvas.fill();
 
 canvas.beginPath();
-canvas.fillStyle = "green";
-canvas.arc(400, 400, 70, 0, 2*Math.PI);
+canvas.fillStyle = "black";
+canvas.arc(400, 400, 80, 0, 2*Math.PI);
 canvas.fill();
 
 canvas.beginPath();
+canvas.fillStyle = "orange";
+canvas.arc(400, 400, 40, 0, 2*Math.PI);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "orange";
 canvas.moveTo(75,50);
 canvas.lineTo(100,75);
 canvas.lineTo(100,25);
