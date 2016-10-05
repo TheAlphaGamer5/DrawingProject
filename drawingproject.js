7da4953799c17f10ff58d4c4dc5c9b8dd8538314
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-10-05 12:13:36 -0700

    fixed it
    all symetrical now

--- drawingproject.js
+++ drawingproject.js
@@ -10,51 +10,55 @@ function drawRectangle(color, xCoord, yCoord) {
   canvas.fillRect(xCoord, yCoord, 800, 800);
 
 }
+//background
 drawRectangle("", 10, 10);
 
+//left side
 canvas.beginPath();
-canvas.fillStyle = "green";
+canvas.fillStyle = "red";
 canvas.moveTo(100,300);
 canvas.lineTo(260,290);
 canvas.lineTo(240,350);
 canvas.fill();
 
 canvas.beginPath();
-canvas.fillStyle = "green";
+canvas.fillStyle = "red";
 canvas.moveTo(400,700);
 canvas.lineTo(260,290);
 canvas.lineTo(240,350);
 canvas.fill();
 
 canvas.beginPath();
-canvas.fillStyle = "green";
+canvas.fillStyle = "red";
 canvas.moveTo(700,300);
 canvas.lineTo(260,290);
 canvas.lineTo(240,350);
 canvas.fill();
 
+
+//right side
 canvas.beginPath();
-canvas.fillStyle = "green";
+canvas.fillStyle = "red";
+canvas.moveTo(700,300);
+canvas.lineTo(540,290);
+canvas.lineTo(560,350);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "red";
+canvas.moveTo(400,700);
+canvas.lineTo(540,290);
+canvas.lineTo(560,350);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "red";
 canvas.moveTo(100,300);
-canvas.lineTo(660,290);
-canvas.lineTo(640,350);
+canvas.lineTo(540,290);
+canvas.lineTo(560,350);
 canvas.fill();
 
-canvas.beginPath();
-canvas.fillStyle = "green";
-canvas.moveTo(700,300);
-canvas.lineTo(560,290);
-canvas.lineTo(540,350);
-canvas.fill();
-
-canvas.beginPath();
-canvas.fillStyle = "green";
-canvas.moveTo(400,700);
-canvas.lineTo(620,290);
-canvas.lineTo(640,350);
-canvas.fill();
-
-
+//cirlces
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
@@ -72,13 +76,7 @@ canvas.arc(400, 400, 40, 0, 2*Math.PI);
 canvas.fill();
 
 
-
-
-
-
-
-
-
+//center
 canvas.beginPath();
 canvas.fillStyle = "orangered";
 canvas.moveTo(280,270);
@@ -99,11 +97,6 @@ canvas.moveTo(520,270);
 canvas.lineTo(450,300);
 canvas.lineTo(500,345);
 canvas.fill();
-
-
-
-
-
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
