8ed8011f3ad0c03920be11bb2f6608b574fc2d92
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-10-07 11:23:47 -0700

    i moved everything up

--- drawingproject.js
+++ drawingproject.js
@@ -18,7 +18,6 @@ canvas.fillStyle = "DimGray";
 canvas.arc(400, 400, 350, 0, 2*Math.PI);
 canvas.fill();
 
-
 canvas.beginPath();
 canvas.fillStyle = "Gray";
 canvas.arc(400, 400, 320, 0, 2*Math.PI);
@@ -30,115 +29,115 @@ canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.moveTo(400,100);
-canvas.lineTo(260,293);
-canvas.lineTo(540,293);
+canvas.moveTo(400,95);
+canvas.lineTo(260,288);
+canvas.lineTo(540,288);
 canvas.fill();
 
 //left side
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.moveTo(100,300);
-canvas.lineTo(260,290);
-canvas.lineTo(240,350);
+canvas.moveTo(100,295);
+canvas.lineTo(260,285);
+canvas.lineTo(240,345);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.moveTo(400,700);
-canvas.lineTo(260,290);
-canvas.lineTo(240,350);
+canvas.moveTo(400,695);
+canvas.lineTo(260,285);
+canvas.lineTo(240,345);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.moveTo(700,300);
-canvas.lineTo(260,290);
-canvas.lineTo(240,350);
+canvas.moveTo(700,295);
+canvas.lineTo(260,285);
+canvas.lineTo(240,345);
 canvas.fill();
 
 
 //right side
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.moveTo(700,300);
-canvas.lineTo(540,290);
-canvas.lineTo(560,350);
+canvas.moveTo(700,295);
+canvas.lineTo(540,285);
+canvas.lineTo(560,345);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.moveTo(400,700);
-canvas.lineTo(540,290);
-canvas.lineTo(560,350);
+canvas.moveTo(400,695);
+canvas.lineTo(540,285);
+canvas.lineTo(560,345);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.moveTo(100,300);
-canvas.lineTo(540,290);
-canvas.lineTo(560,350);
+canvas.moveTo(100,295);
+canvas.lineTo(540,285);
+canvas.lineTo(560,345);
 canvas.fill();
 
 //cirlces
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
-canvas.arc(400, 400, 100, 0, 2*Math.PI);
+canvas.arc(400, 395, 100, 0, 2*Math.PI);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "black";
-canvas.arc(400, 400, 80, 0, 2*Math.PI);
+canvas.arc(400, 395, 80, 0, 2*Math.PI);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
-canvas.arc(400, 400, 40, 0, 2*Math.PI);
+canvas.arc(400, 395, 40, 0, 2*Math.PI);
 canvas.fill();
 
 
 //center
 canvas.beginPath();
 canvas.fillStyle = "orangered";
-canvas.moveTo(280,270);
-canvas.lineTo(350,300);
-canvas.lineTo(300,350);
+canvas.moveTo(280,265);
+canvas.lineTo(350,295);
+canvas.lineTo(300,345);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
-canvas.moveTo(400,170);
-canvas.lineTo(360,290);
-canvas.lineTo(440,290);
+canvas.moveTo(400,165);
+canvas.lineTo(360,285);
+canvas.lineTo(440,285);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
-canvas.moveTo(520,270);
-canvas.lineTo(450,300);
-canvas.lineTo(500,345);
+canvas.moveTo(520,265);
+canvas.lineTo(450,295);
+canvas.lineTo(500,340);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
-canvas.moveTo(220,570);
-canvas.lineTo(450,300);
-canvas.lineTo(500,345);
+canvas.moveTo(220,565);
+canvas.lineTo(450,245);
+canvas.lineTo(500,340);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
-canvas.moveTo(580,570);
-canvas.lineTo(350,300);
-canvas.lineTo(300,350);
+canvas.moveTo(580,565);
+canvas.lineTo(350,245);
+canvas.lineTo(300,345);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orangered";
-canvas.moveTo(400,750);
-canvas.lineTo(360,290);
-canvas.lineTo(440,290);
+canvas.moveTo(400,745);
+canvas.lineTo(360,285);
+canvas.lineTo(440,285);
 canvas.fill();
 
 
